7e8e5e61b9c0371491fbba6b80c102f0d8372a99
Ender <33337309+e3ndr@users.noreply.github.com>
2022-10-19 05:13:26 -0500

    Move the FLASH and SWF players into svelte.

---
+++ src/routes/media/[slug]/[file]/flash.svelte
@@ -0,0 +1,38 @@
+<script context="module">
+	export async function load({ params }) {
+		try {
+			const { slug, file } = params;
+
+			return {
+				props: {
+					slug,
+					file
+				}
+			};
+		} catch (e) {
+			return {
+				status: 500,
+				error: new Error(e)
+			};
+		}
+	}
+</script>
+
+<script>
+	export let slug;
+	export let file;
+</script>
+
+<a href="/media/{slug}">Go Back</a>
+<br />
+<br />
+
+<!-- svelte-ignore a11y-missing-attribute -->
+<object type="application/x-shockwave-flash" data="/flv-player.swf" width="426" height="240">
+	<param name="movie" value="/flv-player.swf" />
+	<param name="allowFullScreen" value="true" />
+	<param
+		name="FlashVars"
+		value="flv={encodeURIComponent(`/media/${slug}/${file}`)}&amp;width=426"
+	/>
+</object>
---
+++ src/routes/media/[slug]/[file]/swf.svelte
@@ -0,0 +1,30 @@
+<script context="module">
+	export async function load({ params }) {
+		try {
+			const { slug, file } = params;
+
+			return {
+				props: {
+					slug,
+					file
+				}
+			};
+		} catch (e) {
+			return {
+				status: 500,
+				error: new Error(e)
+			};
+		}
+	}
+</script>
+
+<script>
+	export let slug;
+	export let file;
+</script>
+
+<a href="/media/{slug}">Go Back</a>
+<br />
+<br />
+
+<embed src="/media/{slug}/{file}" width="426" height="240" />
