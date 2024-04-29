653327c5d1b05505f3f3c7d8df873cc4d724753b
Ender <33337309+e3ndr@users.noreply.github.com>
2024-04-28 19:45:06 -0500

    Rework the table layout while I'm at it...
    much cleaner!

--- src/routes/media/[type]/[slug]/+page.svelte
+++ src/routes/media/[type]/[slug]/+page.svelte
@@ -18,59 +18,70 @@
 
 <br />
 <div>
-	{#each Object.entries(data.files) as [container, files]}
-		{@const supportedPlayers = {
-			flv: ['FLV Player'],
-			swf: ['SWF'],
-			webm: ['HTML5'],
-			mp4: ['HTML5'],
-			matroska: ['HTML5']
-		}[container]}
+	<table>
+		{#each Object.entries(data.files) as [container, files]}
+			{@const supportedPlayers = {
+				flv: ['FLV Player'],
+				swf: ['SWF'],
+				webm: ['HTML5'],
+				mp4: ['HTML5'],
+				matroska: ['HTML5']
+			}[container]}
 
-		<div>
-			<h2>{container.toUpperCase()}</h2>
-			<table>
-				<thead>
+			<thead>
+				<tr>
+					<td class="title-header" colspan="6">
+						<h2>{container.toUpperCase()}</h2>
+					</td>
+				</tr>
+				<tr>
+					<th>Name</th>
+					<th>Codecs</th>
+					<th>Size</th>
+					<th>File</th>
+					<th>Player</th>
+					<th>Notes</th>
+				</tr>
+			</thead>
+			<tbody>
+				{#each files as file}
 					<tr>
-						<th>Name</th>
-						<th>Codecs</th>
-						<th>Size</th>
-						<th>File</th>
-						<th>Player</th>
-						<th>Notes</th>
-					</tr>
-				</thead>
-				<tbody>
-					{#each files as file}
-						<tr>
-							<td>{file.name}</td>
-							<td>{file.codecs.join(', ')}</td>
-							<td>{file.size}</td>
-							<td>
-								<a href={file.file} target="_blank">
-									{file.file.substring(file.file.lastIndexOf('/') + 1)}
+						<td>{file.name}</td>
+						<td>{file.codecs.join(', ')}</td>
+						<td>{file.size}</td>
+						<td>
+							<a href={file.file} target="_blank">
+								{file.file.substring(file.file.lastIndexOf('/') + 1)}
+							</a>
+						</td>
+						<td>
+							{#each supportedPlayers as player}
+								{@const playerType = player.toLowerCase().replace(' ', '-')}
+								<a href="/media/player?file={encodeURIComponent(file.file)}&player={playerType}">
+									{player}
 								</a>
-							</td>
-							<td>
-								{#each supportedPlayers as player}
-									{@const playerType = player.toLowerCase().replace(' ', '-')}
-									<a href="/media/player?file={encodeURIComponent(file.file)}&player={playerType}">
-										{player}
-									</a>
-								{/each}
-							</td>
-							<td>
-								{@html file.notes}
-							</td>
-						</tr>
-					{/each}
-				</tbody>
-			</table>
-		</div>
-	{/each}
+							{/each}
+						</td>
+						<td>
+							{@html file.notes}
+						</td>
+					</tr>
+				{/each}
+				<td colspan="6">
+					<br />
+					<br />
+				</td>
+			</tbody>
+		{/each}
+	</table>
 </div>
 
 <style>
+	.title-header {
+		color: black;
+		background-color: white;
+	}
+
 	thead {
 		text-align: left;
 		color: white;
