78c7fa1744f75c9ee9d95facb35ab827c5d50524
Ender <33337309+e3ndr@users.noreply.github.com>
2024-04-28 20:49:03 -0500

    Achoo.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -58,8 +58,7 @@
 </p>
 <br />
 <div>
-	HTTP:&nbsp;&nbsp;
-	<a href="http://nossl.wmt.e3ndr.xyz" target="_blank">http://nossl.wmt.e3ndr.xyz </a>
+	HTTP Link: <a href="http://nossl.wmt.e3ndr.xyz" target="_blank">http://nossl.wmt.e3ndr.xyz </a>
 	<br />
 	<small>
 		<a href="http://developers.cloudflare.com/ssl/static/origin_ca_ecc_root.pem">
