f745e66898e34b492faec10209e55a1ffa583b09
Ender <33337309+e3ndr@users.noreply.github.com>
2024-04-28 23:39:51 -0500

    Better code.

--- src/routes/tester/misc/+page.svelte
+++ src/routes/tester/misc/+page.svelte
@@ -1,3 +1,10 @@
+<script>
+	const APIS = [
+		{ name: 'navigator', check: 'window.navigator' },
+		{ name: 'navigator.plugins', check: 'window.navigator && window.navigator.plugins' }
+	];
+</script>
+
 <svelte:head>
 	<title>WebMediaTest / Miscellaneous</title>
 </svelte:head>
@@ -81,10 +88,10 @@
 			</tr>
 		</thead>
 		<tbody>
-			{#each [{ name: 'navigator', check: 'window.navigator' }] as { name, check }}
+			{#each APIS as { name, check }}
 				{@const id = `api-${Math.random().toString(28).replace('0.', '')}`}
 				<tr style="color: white;">
-					<td style="font-weight: bold; color: black;"> Navigator </td>
+					<td style="font-weight: bold; color: black;"> {name} </td>
 					<td {id} style="background: black;"> Unknown </td>
 					<!-- prettier-ignore -->
 					<span class="scriptify" style="display: none;">
@@ -109,10 +116,9 @@
 		function doApiTests() {
 			var toTest = document.getElementsByClassName('scriptify');
 			for (var i = 0; i < toTest.length; i++) {
-				var src = toTest[i].innerHTML;
+				var src = toTest[i].innerText;
 				var s = document.createElement('script');
-				console.log(s);
-				s.innerHTML = src;
+				s.innerText = src;
 				document.head.appendChild(s);
 			}
 		}
