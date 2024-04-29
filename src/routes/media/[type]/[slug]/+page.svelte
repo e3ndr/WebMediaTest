3f9b2e708dc9762f3bd27af929dc0bfb3bb3c1f7
Ender <33337309+e3ndr@users.noreply.github.com>
2024-04-28 19:47:30 -0500

    Well, THAT didn't work on the Wii.
    Let's try this instead.

--- src/routes/media/[type]/[slug]/+page.svelte
+++ src/routes/media/[type]/[slug]/+page.svelte
@@ -28,61 +28,52 @@
 				matroska: ['HTML5']
 			}[container]}
 
-			<thead>
+			<tr>
+				<td colspan="6">
+					<h2>{container.toUpperCase()}</h2>
+				</td>
+			</tr>
+			<tr class="header">
+				<th>Name</th>
+				<th>Codecs</th>
+				<th>Size</th>
+				<th>File</th>
+				<th>Player</th>
+				<th>Notes</th>
+			</tr>
+			{#each files as file}
 				<tr>
-					<td class="title-header" colspan="6">
-						<h2>{container.toUpperCase()}</h2>
+					<td>{file.name}</td>
+					<td>{file.codecs.join(', ')}</td>
+					<td>{file.size}</td>
+					<td>
+						<a href={file.file} target="_blank">
+							{file.file.substring(file.file.lastIndexOf('/') + 1)}
+						</a>
+					</td>
+					<td>
+						{#each supportedPlayers as player}
+							{@const playerType = player.toLowerCase().replace(' ', '-')}
+							<a href="/media/player?file={encodeURIComponent(file.file)}&player={playerType}">
+								{player}
+							</a>
+						{/each}
+					</td>
+					<td>
+						{@html file.notes}
 					</td>
 				</tr>
-				<tr>
-					<th>Name</th>
-					<th>Codecs</th>
-					<th>Size</th>
-					<th>File</th>
-					<th>Player</th>
-					<th>Notes</th>
-				</tr>
-			</thead>
-			<tbody>
-				{#each files as file}
-					<tr>
-						<td>{file.name}</td>
-						<td>{file.codecs.join(', ')}</td>
-						<td>{file.size}</td>
-						<td>
-							<a href={file.file} target="_blank">
-								{file.file.substring(file.file.lastIndexOf('/') + 1)}
-							</a>
-						</td>
-						<td>
-							{#each supportedPlayers as player}
-								{@const playerType = player.toLowerCase().replace(' ', '-')}
-								<a href="/media/player?file={encodeURIComponent(file.file)}&player={playerType}">
-									{player}
-								</a>
-							{/each}
-						</td>
-						<td>
-							{@html file.notes}
-						</td>
-					</tr>
-				{/each}
-				<td colspan="6">
-					<br />
-					<br />
-				</td>
-			</tbody>
+			{/each}
+			<td colspan="6">
+				<br />
+				<br />
+			</td>
 		{/each}
 	</table>
 </div>
 
 <style>
-	.title-header {
-		color: black;
-		background-color: white;
-	}
-
-	thead {
+	tr.header {
 		text-align: left;
 		color: white;
 		background-color: black;
