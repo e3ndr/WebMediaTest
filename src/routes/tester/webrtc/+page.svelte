f9307f2937168e69d60d736e62c9f9aa54c008b0
Ender <33337309+e3ndr@users.noreply.github.com>
2024-04-28 22:08:59 -0500

    Local WebRTC call!

--- src/routes/tester/webrtc/+page.svelte
+++ src/routes/tester/webrtc/+page.svelte
@@ -49,3 +49,94 @@
 	<button onclick="startMediaTest()"> Start </button>
 	<button onclick="location.reload();"> End </button>
 </div>
+
+<h2>Local Call</h2>
+<div>
+	<!-- svelte-ignore a11y-media-has-caption -->
+	<video
+		id="local-call-self"
+		style="background: black;"
+		autoplay
+		playsinline
+		controls
+		muted
+		width="256"
+		height="144"
+	/>
+	<video
+		id="local-call-remote"
+		style="background: black;"
+		autoplay
+		playsinline
+		controls
+		muted
+		width="256"
+		height="144"
+	/>
+	<br />
+	<script>
+		function startLocalCall() {
+			try {
+				var constraints = {
+					audio: true,
+					video: true
+				};
+
+				navigator.mediaDevices.getUserMedia(constraints).then(function (selfStream) {
+					console.log('Self:', selfStream);
+					var selfVideo = document.getElementById('local-call-self');
+					var remoteVideo = document.getElementById('local-call-remote');
+					var selfPC = new RTCPeerConnection({});
+					var remotePC = new RTCPeerConnection({});
+
+					if ('srcObject' in selfVideo) {
+						selfVideo.srcObject = selfStream;
+					} else {
+						selfVideo.src = (window.URL || window.webkitURL).createObjectURL(selfStream);
+					}
+
+					selfStream.getTracks().forEach(function (track) {
+						selfPC.addTrack(track, selfStream);
+					});
+
+					selfPC.addEventListener('icecandidate', function (e) {
+						remotePC.addIceCandidate(e.candidate);
+					});
+
+					remotePC.addEventListener('icecandidate', function (e) {
+						selfPC.addIceCandidate(e.candidate);
+					});
+
+					remotePC.addEventListener('track', function (e) {
+						var remoteStream = e.streams[0];
+						console.log('Remote:', remoteStream);
+						if ('srcObject' in remoteVideo) {
+							remoteVideo.srcObject = remoteStream;
+						} else {
+							remoteVideo.src = (window.URL || window.webkitURL).createObjectURL(remoteStream);
+						}
+					});
+
+					remotePC
+						.createOffer({
+							offerToReceiveAudio: 1,
+							offerToReceiveVideo: 1
+						})
+						.then(function (offer) {
+							remotePC.setLocalDescription(offer);
+							selfPC.setRemoteDescription(offer);
+
+							selfPC.createAnswer().then(function (answer) {
+								selfPC.setLocalDescription(answer);
+								remotePC.setRemoteDescription(answer);
+							});
+						});
+				});
+			} catch (e) {
+				alert(e);
+			}
+		}
+	</script>
+	<button onclick="startLocalCall()"> Start </button>
+	<button onclick="location.reload();"> End </button>
+</div>
